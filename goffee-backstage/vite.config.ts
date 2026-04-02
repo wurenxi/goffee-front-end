331f8711cce9487989888f12d613bb1f6b374e13
逆夜 <542014476@qq.com>
2026-04-02 17:21:42 +0800

    test

--- goffee-backstage/vite.config.ts
+++ goffee-backstage/vite.config.ts
@@ -1,6 +1,6 @@
 import vue from '@vitejs/plugin-vue';
 
-import { UserConfig, ConfigEnv, loadEnv, defineConfig } from 'vite';
+import { type UserConfig, type ConfigEnv, loadEnv, defineConfig } from 'vite';
 
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
